2fa57348aea59509a8c43d526aeb820d7d2c37f4
Evgeniy Kanashev <kanashevea@susu.ru>
2024-02-05 11:26:23 +0500

    Добавили процедуру сложения чисел

--- main.c
+++ main.c
@@ -4,9 +4,13 @@
 
 int main()
 {
+    int a, b, c;
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    printf("Здравствуй, мир\n");
+    printf("Введите два целых числа\n");
+    scanf("%d%d", &a, &b);
+    c = a + b;
+    printf("%d + %d = %d\n", a, b, c);
     return 0;
 }
