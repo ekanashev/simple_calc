05b9f1a4a90ef16361bd0366dcb8e45f01918d4a
Evgeniy Kanashev <kanashevea@susu.ru>
2024-02-05 11:09:55 +0500

    Inital commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    printf("Здравствуй, мир\n");
+    return 0;
+}
